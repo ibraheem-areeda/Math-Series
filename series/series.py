41303438acddd271f29878ea2e38f378e214492f
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-05-04 23:45:34 +0000

    fibonacci & lucas & sum_series done and tested

--- series/series.py
+++ series/series.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
+def fibonacci_recursion(n):
     '''
     function take 1 argument which is n
     return the nth value in the fibonacci series
@@ -9,7 +9,41 @@ def fibonacci(n):
     elif n==1:
         return 1
     else:
-        return fibonacci(n-1)+fibonacci(n-2)
+        return fibonacci_recursion(n-1)+fibonacci_recursion(n-2)
+    
+def lucas_recursion(n):
+    '''
+     function take 1 argument which is n
+     return the nth value in the lucas series
+     using recursion
+    '''
+    if n==0:
+        return 2
+    elif n==1:
+        return 1
+    else:
+        return lucas_recursion(n-2) + lucas_recursion(n-1)
+    
+def sum_series ( n , start_val1=None , start_val2=None ):
+    '''
+     function create custom series takes 3 arguments which is
+       n (reqiured) as the nth value in the custom series ,
+       start_val1 (optinal) which is the value at n=0  ,
+       start_val2 (optinal) which is the value at n=1 ,
+     return the nth value in the custom series
+     using recursion
+     if no value passed to the optinal agruments the function will return a fibonacci series
+    '''
+    if n==0:
+        return start_val1 or 0
+    elif n==1:
+        return start_val2 or 1
+    else:
+        return sum_series(n-2 ,start_val1,start_val2 ) + sum_series(n-1,start_val1,start_val2)
+
+if __name__ == "__main__": 
+    sum_series(5)
+
    
     
     
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,60 +1,216 @@
 import pytest
-from series.series import fibonacci
+from series.series import fibonacci_recursion,lucas_recursion,sum_series
 
-def test_edge1():
-    actual = fibonacci(1) #call the function with the test value
+
+##################################### fibonacci ##################################
+
+def test_fibonacci_edge1():
+    actual = fibonacci_recursion(1) #call the function with the test value
     expected = 1 #put the expected result 
     assert actual == expected  #it will return true or false 
 
-def test_edge2():
-    actual = fibonacci(0) 
+def test_fibonacci_edge2():
+    actual = fibonacci_recursion(0) 
     expected = 0 
     assert actual == expected  
 
-def test_two():
-    actual = fibonacci(2) 
+def test_fibonacci_two():
+    actual = fibonacci_recursion(2) 
     expected = 1 
     assert actual == expected  
 
-def test_three():
-    actual = fibonacci(3) 
+def test_fibonacci_three():
+    actual = fibonacci_recursion(3) 
     expected = 2 
     assert actual == expected  
 
-def test_four():
-    actual = fibonacci(4) 
+def test_fibonacci_four():
+    actual = fibonacci_recursion(4) 
     expected = 3 
     assert actual == expected  
 
-def test_five():
-    actual = fibonacci(5) 
+def test_fibonacci_five():
+    actual = fibonacci_recursion(5) 
     expected = 5
     assert actual == expected  
 
-def test_five():
-    actual = fibonacci(5) 
-    expected = 5
-    assert actual == expected  
-
-def test_six():
-    actual = fibonacci(6) 
+def test_fibonacci_six():
+    actual = fibonacci_recursion(6) 
     expected = 8
     assert actual == expected  
 
-def test_seven():
-    actual = fibonacci(7) 
+def test_fibonacci_seven():
+    actual = fibonacci_recursion(7) 
     expected = 13
     assert actual == expected  
 
-def test_ten():
-    actual = fibonacci(10) 
+def test_fibonacci_ten():
+    actual = fibonacci_recursion(10) 
     expected = 55
     assert actual == expected  
 
-def test_eighteen():
-    actual = fibonacci(18) 
+def test_fibonacci_eighteen():
+    actual = fibonacci_recursion(18) 
     expected = 2584
     assert actual == expected  
 
 ##################################### lucas ##################################
+
+def test_lucas_edge1():
+    actual = lucas_recursion(1) 
+    expected = 1 
+    assert actual == expected  
+
+def test_lucas_edge2():
+    actual = lucas_recursion(0) 
+    expected = 2
+    assert actual == expected  
+
+def test_lucas_two():
+    actual = lucas_recursion(2) 
+    expected = 3 
+    assert actual == expected  
+
+def test_lucas_three():
+    actual = lucas_recursion(3) 
+    expected = 4
+    assert actual == expected  
+
+def test_lucas_four():
+    actual = lucas_recursion(4) 
+    expected = 7
+    assert actual == expected  
+
+def test_lucas_five():
+    actual = lucas_recursion(5) 
+    expected = 11
+    assert actual == expected  
+
+def test_lucas_six():
+    actual = lucas_recursion(6) 
+    expected = 18
+    assert actual == expected  
+
+def test_lucas_seven():
+    actual = lucas_recursion(7) 
+    expected = 29
+    assert actual == expected  
+
+def test_lucas_ten():
+    actual = lucas_recursion(10) 
+    expected = 123 
+    assert actual == expected  
+
+def test_lucas_eighteen():
+    actual = lucas_recursion(30) 
+    expected = 1860498   
+    assert actual == expected  
+
+    ##################################### sum_series as default (fibonacci) ##################################
+
+def test_sum_series_as_fibo():
+    actual = sum_series(1) 
+    expected = 1 
+    assert actual == expected  
+
+def test_sum_series_as_fibo():
+    actual = sum_series(0) 
+    expected = 0 
+    assert actual == expected  
+
+def test_sum_series_as_fibo():
+    actual = sum_series(2) 
+    expected = 1 
+    assert actual == expected  
+
+def test_sum_series_as_fibo():
+    actual = sum_series(3) 
+    expected = 2 
+    assert actual == expected  
+
+def test_sum_series_as_fibo():
+    actual = sum_series(4) 
+    expected = 3 
+    assert actual == expected  
+
+def test_sum_series_as_fibo():
+    actual = sum_series(5) 
+    expected = 5
+    assert actual == expected  
+
+def test_sum_series_as_fibo():
+    actual = sum_series(6) 
+    expected = 8
+    assert actual == expected  
+
+def test_sum_series_as_fibo():
+    actual = sum_series(7) 
+    expected = 13
+    assert actual == expected  
+
+def test_sum_series_as_fibo():
+    actual = sum_series(10) 
+    expected = 55
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(18) 
+    expected = 2584
+    assert actual == expected  
+
+    ##################################### sum_series as lucas  ##################################
+
+def test_sum_series_as_lucas():
+    actual = sum_series(1 , 2 , 1) 
+    expected = 1 
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(0 , 2 , 1) 
+    expected = 2 
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(2 , 2 , 1) 
+    expected = 3 
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(3 , 2 , 1) 
+    expected = 4 
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(4, 2 , 1) 
+    expected = 7 
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(5, 2 , 1) 
+    expected = 11
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(6, 2 , 1) 
+    expected = 18
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(7, 2 , 1) 
+    expected = 29
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(10, 2 , 1) 
+    expected = 123
+    assert actual == expected  
+
+def test_sum_series_as_lucas():
+    actual = sum_series(30, 2 , 1) 
+    expected = 1860498
+    assert actual == expected  
+                                                                      
+
+                                                                       
+                                                                       
 
